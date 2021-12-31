4034896079aad9089cbe92bfd806d03dda538982
Jens <dj1jay@xray37.de>
2021-12-31 22:44:11 +0100

    Update tg_update.sh
    Update tgdb.php from Server

--- config/tg_update.sh
+++ config/tg_update.sh
@@ -2,11 +2,11 @@
 
 cd /var/www/html/config/
 
-rm tg.php
+rm tgdb.php
 
 wget https://thueringen.link/Download/tgdb.txt
 
-cp /var/www/html/config/tgdb.txt /var/www/html/config/tgdb.php
+cp /var/www/html/config/tg.sh /var/www/html/config/tgdb.php
 
 rm tgdb.txt
 
