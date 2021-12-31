8c38823d1d6f3c1b156a08b9a6c356c9ac6d4f5a
Jens <dj1jay@xray37.de>
2021-12-31 22:36:53 +0100

    Update 145.275.sh
    MQTT Channel Config 145.275

--- config/channels/145.275.sh
+++ config/channels/145.275.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-mosquitto_pub -t "/djspot/channel/" -m "echo 'AT+DMOSETGROUP=0,145.2750,145.2750,39,2,39,0,1' > /dev/ttyS0"
+mosquitto_pub -h localhost -t "/djspot/channel/" -m "echo 'AT+DMOSETGROUP=0,145.2750,145.2750,39,2,39,0,1' > /dev/ttyS0"
