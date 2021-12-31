a56495aca2b4cf3ac86f2037caecbcaf5053048d
Jens <dj1jay@xray37.de>
2021-12-31 22:38:53 +0100

    Update 432.850.sh
    MQTT Channel Config  432.850 DCS023

--- config/channels/432.850.sh
+++ config/channels/432.850.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mosquitto_pub -t "/djspot/channel/" -m "echo 'AT+DMOSETGROUP=0,432.8500,432.8500,39,2,39,0,1' > /dev/ttyS0"
+mosquitto_pub -h localhost -t "/djspot/channel/" -m "echo 'AT+DMOSETGROUP=0,432.8500,432.8500,39,2,39,0,1' > /dev/ttyS0"
 
