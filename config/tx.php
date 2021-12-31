def39ec0369443c0a8bc0512ff73b49601964c19
Jens <dj1jay@xray37.de>
2021-12-31 22:44:54 +0100

    Create tx.php
    TX icon include

---
+++ config/tx.php
@@ -0,0 +1,11 @@
+<?php
+
+//$bild ="https://thueringen.link/lh/tx.gif"; //Hier gibst du die Bild Url an
+$bild = "tx.gif";
+$flag_thr = "thr_flag.png";
+$width="23" ;//Breite
+$height="20"; //Höhe
+
+$tx     ="<a href= \"$bild\"> <img src=\"$bild\" width=\"$width\" height=\"$height\"  ></a> ";
+$flag   = "<a href= \"$flag_thr\"> <img src=\"$flag_thr\" width=\"$width\" height=\"$height\" ></a>";
+?>
